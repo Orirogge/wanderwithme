3473676ad4f5ad05215aab741f52267d7367b396
Orirogge <ori.roggemans@student.arteveldehs.be>
2017-05-31 11:17:09 +0200

    wip4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$("button").click(function() {
-  $(this).toggleClass("expanded").siblings("div").slideToggle();
-});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("top_button").style.display = "block";
+    } else {
+        document.getElementById("top_button").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
